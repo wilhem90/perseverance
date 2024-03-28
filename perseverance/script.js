6521d80a95a362e41cd3dddd104b5cdb631cbbdc
Wilhem Wundt Maxime <wilhemmaxime97@gmail.com>
2024-03-28 00:23:31 -0300

    Initial commit

---
+++ perseverance/script.js
@@ -0,0 +1,15 @@
+window.addEventListener('DOMContentLoaded', () => {
+    verifyAuth()
+})
+
+async function getComponent(url){
+    const response = await fetch(url)
+    return await response.text()
+}
+
+async function verifyAuth() {
+    if (1 === 1) {
+        const body = document.getElementById('body-content')
+        body.innerHTML = await getComponent('/components/login/login.html')
+    }
+}
